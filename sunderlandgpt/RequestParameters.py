eb4cff702527ce29c146589fc7a71bf118afbeec
paddywardle <paddy.wardle@yahoo.com>
2023-10-08 18:56:17 +0100

    RequestParameters updated

--- sunderlandgpt/RequestParameters.py
+++ sunderlandgpt/RequestParameters.py
@@ -4,7 +4,7 @@ class RequestParameters:
 
     def __init__(self, req: func.HttpRequest):
         
-        self.forum_name = req.params.get('forum_url', "https://www.readytogo.net/smb/threads/prediction-league-sunderland-afc-vs-watford.1617127/")
+        self.forum_name = req.params.get('threadUrl', "https://www.readytogo.net/smb/threads/prediction-league-sunderland-afc-vs-watford.1617127/")
 
     def get_forum_name(self):
 
